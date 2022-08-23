827431ae76ab1728b67fcfb672c5ab40476192cb
André Luis Nascimento Ribeiro <andreluisnr1@hotmail.com>
2022-08-23 09:19:56 -0300

    revisando conteudo de ontem

---
+++ 8_exercicios_revisao/readme.md
@@ -0,0 +1,19 @@
+# Exercício 01
+
+- Escreva três valores em string em um arquivo e exiba no navegador com o console.log().
+- Com aspas duplas, simples e template literals.
+
+# Exercício 02
+- Escreva três valores em number em um arquiavo e exiba no navegador com o console.log().
+- Com números inteiros, números com casa decimal e por aritmética.
+
+# Exercício 03
+- Escreva três comparações com boolean.
+- Uma com maior, menor ou igual e diferente.
+
+# Exercício 04
+- Escreva três comparações com operadores lógicos.
+- Com and, or e not.
+
+# Exercício 05
+- Faça uma operação que emita NaN no console no navegador;
---
+++ 8_exercicios_revisao/scripts.js
@@ -0,0 +1,39 @@
+// 01
+console.log('Exercício 01')
+console.log("André Luis")
+console.log('Está estudando JavaScript')
+console.log(`Para se tornar um Dev FullStack`)
+console.log('')
+
+// 02
+console.log('Exercício 02')
+console.log(10)
+console.log(52.76)
+console.log(2 * (10 + 15 - 2) / 5)
+console.log('')
+
+// 03
+console.log('Exercício 03')
+console.log(10 + 10 > 20)
+console.log(502 <= 664)
+console.log("André" != "Andre")
+console.log('')
+
+// 04
+console.log('Exercício 04')
+console.log(10 + 5 > 14 && 20 < 4)
+console.log(10 >= 9 || 20 / 2 == 10)
+console.log(!("André" == "André"))
+console.log('')
+
+// 05
+console.log('Exercício 05')
+console.log(15 / "João")
+console.log(405 * "Não é um número")
+console.log('')
+
+
+
+
+
+
